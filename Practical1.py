ee91a0ff4476a9e377f89e9a6a42e2ec0af273e4
Maxym Bogulskyi <99243052+Sskywwy@users.noreply.github.com>
2024-09-04 19:36:58 +0300

    Add files via upload

---
+++ Practical1.py
@@ -0,0 +1,8 @@
+a = 'Hello world'
+name='Maxym'
+surname='Bogulskyi'
+age=17
+print('Value а:',a,';','Type:',type(a))
+print('Value name:',name,';','Type:',type(name))
+print('Value surname:',surname,';','Type:',type(surname))
+print('Value age:',age,';','Type:',type(age))
